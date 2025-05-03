309819902f41fbf226d28d8c67f4a3a9ddc5ab36
LeoSong0911 <songyy09@hotmail.com>
2025-05-03 22:32:47 +0800

    add first verilog testbench with gtkwave

--- test.v
+++ test.v
@@ -1,5 +1,13 @@
-module moduleName (
-    ports
-);
-    
+module test;
+  reg clk = 0;
+  reg rst = 1;
+  always #5 clk = ~clk;
+
+  initial begin
+    $dumpfile("wave.vcd");
+    $dumpvars(0, test);
+
+    #10 rst = 0;
+    #100 $finish;
+  end
 endmodule
